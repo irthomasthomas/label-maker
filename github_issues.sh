ad251a2e7fdc43072c78123c434a37bd90e88954
Thomas <irthomasthomas@gmail.com>
2024-01-08 21:58:05 +0000

    Refactor code to improve readability and remove unnecessary comments and print statements.

--- github_issues.sh
+++ github_issues.sh
@@ -6,41 +6,44 @@ send_note_to_github() {
     local TITLE="$1"
     local URL="$2"
     local DESCRIPTION="$3"
+    local labels="$4"
+    local gh_markdown_highlight_generated_labels="$5"
     local BODY
     local task_list
-    local labels_csv
     local issue_url
+    local labels_csv
+    echo >&2
+    echo "send_note_to_github" >&2
+    echo "gh_markdown_highlight_generated_labels: $gh_markdown_highlight_generated_labels" >&2
 
+    echo >&2
     if [ -z "$TITLE" ]; then
         TITLE="$(llm "generate a title from this url:$URL:quote:$DESCRIPTION" -o temperature 0.1)"
     fi
-
+    
     task_list="- [ ] [${TITLE}](${URL})"
+    suggested_labels="#### Suggested labels
+#### $gh_markdown_highlight_generated_labels"
+    
     BODY="$task_list
 
-    $DESCRIPTION"
-    labels_csv=$(get_labels "$TITLE" "$URL" "$DESCRIPTION")
-    # print to stderr
-    echo "title: $TITLE
-url: $URL 
-description: $DESCRIPTION
-labels: $labels_csv
-" >&2
-    # issue_url=$(gh issue create --title "$TITLE" --body "$BODY" --label "$labels_csv")
-    # echo "$issue_url"
+$DESCRIPTION
+
+$suggested_labels"
+    labels_csv=$(echo "$labels" | tr '\n' ',' | sed 's/.$//')
+    echo "labels_csv: $labels_csv" >&2
+    
+    issue_url=$(gh issue create --title "$TITLE" --body "$BODY" --label "$labels_csv" --web)
+    echo "$issue_url"
 }
 
 
-get_labels() {
+get_labels_json() {
     local TITLE="$1"
     local URL="$2"
     local DESCRIPTION="$3"
-    local labels
-    local labels_csv
-    labels=$(python /home/thomas/Development/Projects/llm/ai-issues/llm_label_maker/label_maker.py --url "$URL" --title "$TITLE" --description "$DESCRIPTION")
-    labels_csv=$(echo "$labels" | tr -d [])
-    labels_csv=$(echo "$labels_csv" | tr -d \' | tr -d ' ')
-    echo "$labels_csv"
+    local labels=$(python /home/thomas/Development/Projects/llm/label-maker/label_maker.py --url "$URL" --title "$TITLE" --description "$DESCRIPTION")
+    echo "$labels"
 }
 
 # Main execution
@@ -49,4 +52,14 @@ if [ "$#" -ne 3 ]; then
     exit 1
 fi
 
-send_note_to_github "$1" "$2" "$3"
+TITLE="$1"
+URL="$2"
+DESCRIPTION="$3"
+
+labels_json=$(get_labels_json "$TITLE" "$URL" "$DESCRIPTION")
+generate_labels=$(echo "$labels_json" | jq '.generated_labels | to_entries[]')
+
+picked_labels=$(echo "$labels_json" | jq '.picked_labels | to_entries[] | select(.value == true) | .key')
+gh_markdown_highlight_generated_labels=$(echo "$generate_labels" | tr '\n' ' ' | sed 's/.$//')
+
+send_note_to_github "$TITLE" "$URL" "$DESCRIPTION" "$picked_labels" "$gh_markdown_highlight_generated_labels"
--- nyxt_to_github.sh
+++ nyxt_to_github.sh
@@ -1,6 +1,40 @@
 #!/bin/bash
-
 # nyxt_to_github.sh
+
+create_pipes() {
+    echo "create pipes"
+    if [ ! -p "$pipe_title" ]; then
+        mkfifo "$pipe_title" && echo "Created named pipe: $pipe_title" >&2
+    fi
+
+    if [ ! -p "$pipe_url" ]; then
+        mkfifo "$pipe_url" && echo "Created named pipe: $pipe_url" >&2
+    fi
+
+    if [ ! -p "$pipe_selection" ]; then
+        mkfifo "$pipe_selection"   && echo "Created named pipe: $pipe_selection" >&2
+    fi
+
+    if [ ! -d "$target_directory" ]; then
+        echo "Cannot find directory $target_directory" >&2
+        exit 1
+    fi
+}
+
+monitor_nyxt_to_gh_pipes() {
+    local TITLE
+    local URL
+    local SELECTION
+    local issue_url
+    echo "monitoring pipes" >&2
+
+    TITLE="$(cat ${pipe_title})"
+    URL="$(cat $pipe_url)"
+    SELECTION="$(cat $pipe_selection)"
+
+    echo "TITLE: $TITLE\nURL: $URL\n SELECTION: $SELECTION\n" >&2
+    /home/thomas/Development/Projects/llm/label-maker/github_issues.sh "$TITLE" "$URL" "$SELECTION"
+}
 
 target_project="undecidability"
 target_directory="$HOME/$target_project"
@@ -11,41 +45,6 @@ pipe_title="/tmp/nyxt_title"
 pipe_url="/tmp/nyxt_url"
 pipe_selection="/tmp/nyxt_selection"
 
-create_pipes() {
-    echo "create pipes"
-    if [ ! -p "$pipe_title" ]; then
-        mkfifo "$pipe_title" && echo "Created named pipe: $pipe_title"
-    fi
-
-    if [ ! -p "$pipe_url" ]; then
-        mkfifo "$pipe_url" && echo "Created named pipe: $pipe_url"
-    fi
-
-    if [ ! -p "$pipe_selection" ]; then
-        mkfifo "$pipe_selection"   && echo "Created named pipe: $pipe_selection"
-    fi
-
-    if [ ! -d "$target_directory" ]; then
-        echo "Cannot find directory $target_directory"
-        exit 1
-    fi
-}
-
-monitor_nyxt_to_gh_pipes() {
-    local TITLE
-    local URL
-    local SELECTION
-    local issue_url
-
-    TITLE="$(cat ${pipe_title})"
-    URL="$(cat $pipe_url)"
-    SELECTION="$(cat $pipe_selection)"
-    # echo "TITLE: $TITLE\nURL: $URL\n SELECTION: $SELECTION\n"
-    sh /home/thomas/Development/Projects/llm/ai-issues/github_issues.sh "$TITLE" "$URL" "$SELECTION"
-    # issue_url=$(/home/thomas/Development/Projects/llm/ai-issues/github_issues.sh "$TITLE" "$URL" "$SELECTION")
-    # echo "issue_url: $issue_url"
-    # nyxt "$issue_url"
-}
 
 # Main loop
 echo "Starting nyxt-to-gh.sh"
